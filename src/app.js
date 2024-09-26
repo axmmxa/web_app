d9776e4c308ff218002b560ce3e7dd62d4e532fc
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2024-09-26 13:31:51 +0200

    dynamic routes added

--- src/app.js
+++ src/app.js
@@ -8,18 +8,29 @@ server.listen(PORT, ()=> {
 
 console.log("App started")
 
-//404 Handler 
-server.use( (req, res) => {
-    res.status(404).json({
-        status:"Seite existiert nicht! 404 Error"
-    })
-})
+
 
 //Routes 
 server.get("/", (req, res) => {
     res.send("Main Route")
 })
 
+
+
 server.get("/profile", (req, res) =>{
     res.send("Profile Route")
+})
+
+
+server.get("/profile/:userid", (req, res) =>{
+    console.log(req.params.userid)
+    res.send("Profile Route")
+})
+
+
+//404 Handler 
+server.use( (req, res) => {
+    res.status(404).json({
+        status:"Seite existiert nicht! 404 Error"
+    })
 })
